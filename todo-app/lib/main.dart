608f29d4ebccd8eafab3f4eecc57843e18d1fae0
Olubukun Samuel <samuelolubukun@gmail.com>
2023-10-09 13:17:44 -0700

    update for main.dart

--- todo-app/lib/main.dart
+++ todo-app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class Todo {
+  String text;
+  bool completed;
+
+  Todo({
+    required this.text,
+    this.completed = false,
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -20,29 +31,68 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
-  List<String> _todos = [];
+  late SharedPreferences _prefs;
+  List<Todo> _todos = [];
+  String newTodo = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  _initData() async {
+    await _initSharedPreferences();
+    _loadData();
+  }
+
+  Future<void> _initSharedPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  _loadData() async {
+    _prefs = await SharedPreferences.getInstance();
+    List<String> todoStrings = _prefs.getStringList('todos') ?? [];
+    setState(() {
+      _todos = todoStrings.map((text) => Todo(text: text)).toList();
+    });
+  }
+
+  _saveData() async {
+    List<String> todoStrings = _todos.map((todo) => todo.text).toList();
+    await _prefs.setStringList('todos', todoStrings);
+  }
 
   void _addTodo() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        String newTodo = "";
         return AlertDialog(
           title: Text("Add a new To-Do"),
           content: TextField(
             onChanged: (value) {
-              newTodo = value;
+              setState(() {
+                newTodo = value;
+              });
             },
+            decoration: InputDecoration(
+              hintText: 'Enter your to-do',
+            ),
           ),
           actions: [
             ElevatedButton(
               onPressed: () {
-                setState(() {
-                  if (newTodo.isNotEmpty) {
-                    _todos.add(newTodo);
-                  }
-                  Navigator.pop(context);
-                });
+                if (newTodo.isNotEmpty) {
+                  setState(() {
+                    _todos.add(Todo(text: newTodo));
+                    _saveData();
+                    Navigator.pop(context);
+                    _showSnackBar("To-Do added successfully");
+                    newTodo = "";
+                  });
+                } else {
+                  _showSnackBar("Please enter a to-do");
+                }
               },
               child: Text("Add"),
             ),
@@ -50,6 +100,44 @@ class _TodoListScreenState extends State<TodoListScreen> {
         );
       },
     );
+  }
+
+  void _deleteTodo(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Delete To-Do"),
+          content: Text("Are you sure you want to delete this To-Do?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _todos.removeAt(index);
+                  _saveData();
+                  Navigator.pop(context);
+                  _showSnackBar("To-Do deleted");
+                });
+              },
+              child: Text("Delete"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 2),
+    ));
   }
 
   @override
@@ -67,17 +155,58 @@ class _TodoListScreenState extends State<TodoListScreen> {
             margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
             child: ListTile(
               title: Text(
-                _todos[index],
-                style: TextStyle(fontSize: 18),
+                _todos[index].text,
+                style: TextStyle(
+                  fontSize: 18,
+                  decoration: _todos[index].completed
+                      ? TextDecoration.lineThrough
+                      : null,
+                ),
               ),
               trailing: IconButton(
                 icon: Icon(Icons.delete),
                 onPressed: () {
-                  setState(() {
-                    _todos.removeAt(index);
-                  });
+                  _deleteTodo(index);
                 },
               ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Edit To-Do"),
+                      content: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            _todos[index].text = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Edit your to-do',
+                        ),
+                        controller:
+                            TextEditingController(text: _todos[index].text),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            _saveData();
+                            _showSnackBar("To-Do edited");
+                          },
+                          child: Text("Save"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              onLongPress: () {
+                setState(() {
+                  _todos[index].completed = !_todos[index].completed;
+                  _saveData();
+                });
+              },
             ),
           );
         },
